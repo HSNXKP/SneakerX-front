d3037c7129d32a0d15ae43e2063befb261293cd7
wdd <2979554858@qq.com>
2023-03-12 23:09:22 +0800

    修复Login操作

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -11,7 +11,8 @@
 					<el-input v-model="loginForm.username" prefix-icon="el-icon-user-solid"></el-input>
 				</el-form-item>
 				<el-form-item prop="password">
-					<el-input v-model="loginForm.password" prefix-icon="el-icon-lock" show-password @keyup.native.enter="login"></el-input>
+					<el-input v-model="loginForm.password" prefix-icon="el-icon-lock" show-password
+						@keyup.native.enter="login"></el-input>
 				</el-form-item>
 				<el-form-item class="btns">
 					<el-button type="primary" @click="login">登录</el-button>
@@ -23,105 +24,107 @@
 </template>
 
 <script>
-	import {login} from "@/api/login";
-  import {mapState} from 'vuex'
+import { login } from "@/api/login";
+import { mapState } from 'vuex'
 
-	export default {
-		name: "Login",
-		data() {
-			return {
-				loginForm: {
-					username: 'admin',
-					password: '123456'
-				},
-				loginFormRules: {
-					username: [
-						{required: true, message: '请输入用户名', trigger: 'blur'},
-					],
-					password: [
-						{required: true, message: '请输入密码', trigger: 'blur'},
-					]
-				}
-			}
-		},
-    computed: {
-    ...mapState(['user'])
-  },
-    methods: {
-			resetLoginForm() {
-				this.$refs.loginFormRef.resetFields();
+export default {
+	name: "Login",
+	data() {
+		return {
+			loginForm: {
+				username: 'admin',
+				password: '123456'
 			},
-			login() {
-				this.$refs.loginFormRef.validate(valid => {
-					if (valid) {
-						login(this.loginForm).then(res => {
-							if (res.code === 200) {
-								this.msgSuccess(res.msg)
-                window.localStorage.setItem('userInfo', res.data.user)
-								window.localStorage.setItem('adminToken', res.data.token)
-                this.$store.commit('user', res.data.user)
-                this.$router.push('/home')
-              } else {
-								this.msgError(res.msg)
-							}
-						}).catch(() => {
-							this.msgError("请求失败")
-						})
-					}
-				})
+			loginFormRules: {
+				username: [
+					{ required: true, message: '请输入用户名', trigger: 'blur' },
+				],
+				password: [
+					{ required: true, message: '请输入密码', trigger: 'blur' },
+				]
 			}
 		}
+	},
+	computed: {
+		...mapState(['user'])
+	},
+	methods: {
+		resetLoginForm() {
+			this.$refs.loginFormRef.resetFields();
+		},
+		login() {
+			this.$refs.loginFormRef.validate(valid => {
+				if (valid) {
+					login(this.loginForm).then(res => {
+						if (res.code === 200) {
+							this.msgSuccess(res.msg)
+							window.localStorage.setItem('userInfo', res.data.user)
+							window.localStorage.setItem('adminToken', res.data.token)
+							this.$store.commit('user', res.data.user)
+							this.$router.push('/home')
+							this.$router.push('/home')
+							this.$router.push('/home')
+						} else {
+							this.msgError(res.msg)
+						}
+					}).catch(() => {
+						this.msgError("请求失败")
+					})
+				}
+			})
+		}
 	}
+}
 </script>
 
 <style scoped>
-	.login_container {
-		box-sizing: unset !important;
-		height: 100%;
-		background-color: #2b4b6b;
-	}
+.login_container {
+	box-sizing: unset !important;
+	height: 100%;
+	background-color: #2b4b6b;
+}
 
-	.login_box {
-		width: 450px;
-		height: 300px;
-		background-color: #fff;
-		border-radius: 3px;
-		position: absolute;
-		left: 50%;
-		top: 50%;
-		transform: translate(-50%, -50%);
-	}
+.login_box {
+	width: 450px;
+	height: 300px;
+	background-color: #fff;
+	border-radius: 3px;
+	position: absolute;
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%, -50%);
+}
 
-	.login_box .avatar_box {
-		height: 130px;
-		width: 130px;
-		border: 1px solid #eee;
-		border-radius: 50%;
-		padding: 10px;
-		box-shadow: 0 0 10px #ddd;
-		position: absolute;
-		left: 50%;
-		transform: translate(-50%, -50%);
-		background-color: #fff;
-	}
+.login_box .avatar_box {
+	height: 130px;
+	width: 130px;
+	border: 1px solid #eee;
+	border-radius: 50%;
+	padding: 10px;
+	box-shadow: 0 0 10px #ddd;
+	position: absolute;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	background-color: #fff;
+}
 
-	.login_box .avatar_box img {
-		width: 100%;
-		height: 100%;
-		border-radius: 50%;
-		background-color: #eee;
-	}
+.login_box .avatar_box img {
+	width: 100%;
+	height: 100%;
+	border-radius: 50%;
+	background-color: #eee;
+}
 
-	.login_form {
-		position: absolute;
-		bottom: 0;
-		width: 100%;
-		padding: 0 20px;
-		box-sizing: border-box;
-	}
+.login_form {
+	position: absolute;
+	bottom: 0;
+	width: 100%;
+	padding: 0 20px;
+	box-sizing: border-box;
+}
 
-	.btns {
-		display: flex;
-		justify-content: flex-end;
-	}
+.btns {
+	display: flex;
+	justify-content: flex-end;
+}
 </style>
