5906f665f71a0403c8165ff9a1745fb7fc5ce1a1
wdd <2979554858@qq.com>
2023-03-19 17:43:44 +0800

    增加球鞋路由

---
+++ src/views/sneaker/Sneaker.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="ui top attached segment" style="text-align: center">
+			<h2 class="m-text-500">我的动态</h2>
+		</div>
+		<div class="ui attached segment m-padding-bottom-large">
+            s s s 测试
+            </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Sneaker",
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scope>
+
+</style>
