a5d9f8d62b709b4f180c616b974e715887390381
wdd <2979554858@qq.com>
2023-03-28 23:29:39 +0800

    增加动态中的user

--- src/views/blog/Blog.vue
+++ src/views/blog/Blog.vue
@@ -70,12 +70,11 @@
 		<!--博客信息-->
 		<div class="ui attached positive message">
 			<ul class="list">
-				<li>作者：{{ $store.state.introduction.name }}
-					<router-link to="/about">（联系作者）</router-link>
+				<li>作者：{{ blog.user.username }}
 				</li>
 				<li>发表时间：{{ blog.createTime | dateFormat('YYYY-MM-DD HH:mm') }}</li>
 				<li>最后修改：{{ blog.updateTime | dateFormat('YYYY-MM-DD HH:mm') }}</li>
-				<li>本站点采用<a href="https://creativecommons.org/licenses/by/4.0/" target="_blank"> 署名 4.0 国际 (CC BY 4.0) </a>创作共享协议。可自由转载、引用，并且允许商业性使用。但需署名作者且注明文章出处。</li>
+				<li>本站点采用<a href="https://creativecommons.org/licenses/by/4.0/" target="_blank"> 署名 4.0 国际 (CC BY 4.0) </a>创作共享协议</li>
 			</ul>
 		</div>
 		<!--评论-->
