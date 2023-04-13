a94a8d848fa44520008377d60bad1238d1980c94
wdd <2979554858@qq.com>
2023-04-14 01:35:22 +0800

    增加购物车结算

--- src/views/product/SettleAccount.vue
+++ src/views/product/SettleAccount.vue
@@ -6,94 +6,161 @@
     <!-- <div class="ui attached segment m-padding-bottom-large" >
       <el-empty description="暂无订单，快去下单吧！" ></el-empty>
     </div> -->
+    <div class="ui attached segment m-padding-bottom-large" v-if="this.cartList.length === 0">
+      <el-empty description="暂无商品 赶紧加入购物车吧！"></el-empty>
+    </div>
     <div class="ui attached segment m-padding-bottom-large">
-      <!-- v-if 做判断先判断空 在使用v-else 要是先判断v-if="!order.children === null" 的情况会出问题-->
-      <div class="ui attached segment m-padding-bottom-large" >
-        <div class="orderInfo">
-          <span >商品名称:ss</span>
-        </div>
-        <el-card shadow="always" class="orderInfo">
-          <img :src="url" />
-        </el-card>
-
-        <div class="ui divider"></div>
-    
-        <div class="orderDetailInfo"> 
-          商品尺码 : [ss]
-        </div>
-        <div class="orderDetailInfo">
-          <span class="orderPrice">
-            商品单价 :ss
-          </span>
-        </div>
-        <div class="orderDetailInfo">
-          商品数量 :  xss
-        </div>
-        <div class="orderDetailInfo">
-          <span class="orderPrice">
-            商品总价格 : sss
-          </span>
-        </div>
-   
-        <!-- <div class="orderDetailInfo">
-            订单备注 :ss
-          <el-input v-if="order.status === 0" type="textarea" placeholder="订单备注:给个鞋包呗！(当然肯定不会给)" v-model="orderRemarks"></el-input>
-        </div> -->
-  
-      </div>
-      <div class="ui attached segment m-padding-bottom-large" >
-        <div class="orderInfo">
-          <span >商品名称:ss</span>
-        </div>
-        <el-card shadow="always" class="orderInfo">
-          <img :src="url" />
-        </el-card>
-
-        <div class="ui divider"></div>
-    
-        <div class="orderDetailInfo"> 
-          商品尺码 : [ss]
-        </div>
-        <div class="orderDetailInfo">
-          <span class="orderPrice">
-            商品单价 :ss
-          </span>
-        </div>
-        <div class="orderDetailInfo">
-          商品数量 :  xss
-        </div>
-        <div class="orderDetailInfo">
-          <span class="orderPrice">
-            商品总价格 : sss
-          </span>
-        </div>
-   
-        <!-- <div class="orderDetailInfo">
-            订单备注 :ss
-          <el-input v-if="order.status === 0" type="textarea" placeholder="订单备注:给个鞋包呗！(当然肯定不会给)" v-model="orderRemarks"></el-input>
-        </div> -->
+      <div>
+        <el-select v-model="addressId" placeholder="请选择收获地址">
+          <el-option
+            v-for="item in address"
+            :key="item.id"
+            :label="item.name + item.addressDetail"
+            :value="item.id"
+          >
+            <span style="float: left">{{ item.name + item.phone }}</span>
+            <span style="float: right; color: #8492a6; font-size: 13px">{{
+              item.addressDetail
+            }}</span>
+          </el-option>
+          <el-option :key="''" :value="''" v-if="address.length == 0">
+            <span style="float: left">没有收货地址</span>
+          </el-option>
+        </el-select>
+              <el-button
+          style="margin-left: 5px"
+          icon="el-icon-location-information"
+          size="mini"
+          circle
+          @click="addAddressDialg"
+        ></el-button>
       </div>
 
-      <div class="orderInfo">
-          <el-button type="success" >支付订单</el-button>
-          <el-button type="danger">取消结算</el-button>
+      <!--添加地址框-->
+      <el-dialog
+        title="添加新的收货地址"
+        width="50%"
+        :visible.sync="addAddressDialgVisible"
+        :before-close="dialogVisibleClosed"
+      >
+        <!--内容主体-->
+        <el-form
+          label-width="80px"
+          ref="formRef"
+          :model="form"
+          :rules="formRules"
+        >
+          <el-form-item label="收货姓名" prop="name">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="收货电话" prop="phone">
+            <el-input v-model="form.phone"></el-input>
+          </el-form-item>
+          <el-form-item label="详情省市" prop="address">
+            <el-cascader
+              :options="options"
+              v-model="selectedAddress"
+              @change="addressChange"
+            ></el-cascader>
+          </el-form-item>
+          <el-form-item label="详情地址" prop="addressDetail">
+            <el-input v-model="form.addressDetail"></el-input>
+          </el-form-item>
+          <el-form-item style="float: left" label="默认地址">
+            <el-switch v-model="form.isDefaultAddress"></el-switch>
+          </el-form-item>
+        </el-form>
+        <!--底部-->
+        <span slot="footer">
+          <el-button type="primary" @click="saveAddress">确 定</el-button>
+          <el-button @click="dialogVisibleClosed">取 消</el-button>
+        </span>
+      </el-dialog>
+
+      <div class="ui divider"></div>
+      <div v-for="(cart, index) in cartList" :key="index">
+        <div class="orderInfo">
+          <el-card class="imageInfo">
+              <el-image style="width: 100%; height: 100%; padding: 0 0 0 0" :src="cart.image"></el-image>
+          </el-card>
+          <div>
+            <div class="orderDetail">商品：{{ cart.name }}</div>
+            <div class="orderDetail">尺码：{{ cart.size }}</div>
+            <div class="orderDetail">货号：{{ cart.code }}</div>
+            <div class="orderDetail">
+              数量：
+              <a>
+                <i
+                  @click="downQuantity(cart.id)"
+                  class="el-icon-remove-outline"
+                ></i>
+              </a>
+              {{ cart.quantity }}
+              <a>
+                <i
+                  @click="addQuantity(cart.id)"
+                  class="el-icon-circle-plus-outline"
+                ></i>
+              </a>
+            </div>
+            <div class="productNumber">
+              <span style="color: red">￥{{ cart.price }}</span>
+            </div>
+          </div>
         </div>
-      
+      </div>
+      <div class="ui divider"></div>
+      <span class="amountDetail"
+        >合计:
+        <span class="amount"> ￥{{ this.amount }} </span>
+      </span>
+      <div class="amounts">
+        <el-button type="danger" @click="settleAccount" size="mini">提交订单</el-button>
+      </div>
+      <div></div>
     </div>
   </div>
 </template>
 
 <script>
-
+import { regionData, CodeToText } from "element-china-area-data";
+import { saveAddress, getAddressList } from "@/api/address";
+import { checkPhone } from "@/common/reg";
 import { getCartList } from '@/api/cart';
 import { mapState } from "vuex";
+import { addQuantity, downQuantity} from "@/api/cart";
 
 export default {
   name: "SettleAccount",
   data() {
     return {
-      url:'https://img.picgo.net/2023/04/06/Nike-SB-Dunk-Low-White-Gum-Product258f5013cf9724a3.webp',
-      cartList:[]
+      cartList:[],
+      amount:0,
+      form: {
+        name: "",
+        phone: "",
+        address: "",
+        addressDetail: "",
+        isDefaultAddress: false,
+      },
+      address : [],
+      addressId: "",
+      options: regionData,
+      selectedAddress: [],
+       // 校验
+       formRules: {
+        name: [
+          { required: true, message: "请输入收货人姓名", trigger: "blur" },
+        ],
+        phone: [
+          { required: true, message: "请输入收货人电话", trigger: "blur" },
+          { validator: checkPhone },
+        ],
+        addressDetail: [
+          { required: true, message: "请输入收货人详细地址", trigger: "blur" },
+        ],
+      },
+      addAddressDialgVisible: false,
     };
   },
   computed: {
@@ -101,6 +168,7 @@ export default {
   },
   created(){
     this.getCartList()
+    this.getAddressList()
   },
   methods:{
     getCartList(){
@@ -108,12 +176,102 @@ export default {
         const userId = this.user.id
         getCartList(token,userId).then(res => {
           if (res.code === 200) {
-            this.cartList = res.dataa
-            this.msgSuccess(res.msg);
+            this.cartList = res.data
+            this.computeAmount(res.data)
           }else{
             this.msgError(res.msg);
           }
         })
+    },
+      // 计算总价
+      computeAmount(data) {
+      var amount = 0;
+      for (var i = 0; i < data.length; i++) {
+          amount = amount + data[i].amount;
+          this.amount = amount;
+      }
+    },
+    // 保存地址
+    saveAddress() {
+      const token = window.localStorage.getItem("adminToken");
+      // 获得user的id
+      const id = this.user.id;
+      // 转换成Long类型
+      saveAddress(token, this.form, id).then((res) => {
+        if (res.code == 200) {
+          this.msgSuccess(res.msg);
+          // 清空表单 因为没有ref 所以只能这样清空
+          this.form = {};
+          // 关闭弹窗
+          this.dialogVisibleClosed();
+          // 更新收货地址
+          this.getAddressList();
+        } else {
+          this.msgError(res.msg);
+        }
+      });
+    },
+    // 省市区联动
+    addressChange(arr) {
+      // console.log(this.selectedOptions) //  Proxy {0: '120000', 1: '120100', 2: '120101'}
+      // console.log(arr) //  Proxy {0: '120000', 1: '120100', 2: '120101'}  和上句一样
+      // addressText为el-cascader的值 北京市 市辖区 朝阳区
+      var addressText =
+        CodeToText[arr[0]] + CodeToText[arr[1]] + CodeToText[arr[2]];
+      this.form.address = addressText;
+    },
+    // 获取当前用户的收货地址
+    getAddressList() {
+      const id = this.user.id;
+      const token = window.localStorage.getItem("adminToken");
+      getAddressList(token, id).then((res) => {
+        if (res.code == 200) {
+          this.formatData(res.data);
+          this.address = res.data;
+        } else {
+          this.msgError(res.msg);
+        }
+      });
+    },
+    // 将当前登录的用户Id查询出来的收货地址放到默认的地址上面去
+    formatData(data) {
+      for (var i = 0; i < data.length; i++) {
+        if (data[i].isDefaultAddress === true) {
+          this.addressId = data[i].id;
+        }
+      }
+      return data;
+    },
+    addAddressDialg() {
+      this.addAddressDialgVisible = true;
+    },
+    dialogVisibleClosed() {
+      this.addAddressDialgVisible = false;
+    },
+    addQuantity(id) {
+      const token = window.localStorage.getItem("adminToken");
+      const userId = this.user.id;
+      addQuantity(token, id, userId).then((res) => {
+        if (res.code == 200) {
+          this.getCartList();
+        } else {
+          this.msgError(res.msg);
+        }
+      });
+    },
+    downQuantity(id) {
+      const token = window.localStorage.getItem("adminToken");
+      const userId = this.user.id;
+      downQuantity(token, id, userId).then((res) => {
+        if (res.code == 200) {
+          this.getCartList();
+        } else {
+          this.msgError(res.msg);
+        }
+      });
+    },
+    settleAccount(){
+      this.$router.push({path:'/order'})
     }
   }
 };
@@ -122,17 +280,52 @@ export default {
 <style scoped>
 
 .orderInfo {
-  text-align: center;
-  margin-top: 10px;
+  margin-bottom: 5px;
+  display: flex;
 }
-.orderDetailInfo {
-  margin-top: 10px;
+
+.orderDetail {
+  font-size: 12px;
+  margin-left: 10px;
   color: #999;
 }
 
+.productCode {
+  font-size: 12px;
+  margin-left: 12.5px;
+  color: #999;
+  zoom: 0.8;
+}
 
-.orderPrice {
+.productNumber {
   font-size: 14px;
+  margin-left: 10px;
+  color: #999;
+}
+
+.amount {
+  font-size: 20px;
   color: red;
+}
+
+.amountDetail {
+  margin-top: 5px;
+  font-size: 14px;
+  margin-left: 10px;
+  float: right;
+}
+
+.amounts {
+  text-align: right;
+}
+
+.productName {
+  font-size: 12px;
+  margin-left: 10px;
+}
+.imageInfo {
+  margin-left: 10px;
+  width: 185px;
+  height: 120px;
 }
 </style>
